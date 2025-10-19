bd371cc8abc73dc4dbb26ed5b70313ae8c3e5892
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-19 23:09:09 +0300

    Get Decorator

---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get() //Get Decoractor:Method Decorator from nestjs common
+  getUsers() {
+    //Method to typically return a list of users to the user consuming this endpoint.
+    return [{ userName: 'Anson', email: 'anson@anson.com' }];
+  }
+  @Get('posts')
+  getUsersPosts() {
+    return [
+      {
+        userName: 'Anson',
+        email: 'anson@anson.com',
+        posts: [
+          {
+            id: 1,
+            title: 'post 1',
+          },
+          {
+            id: 2,
+            title: 'post 2',
+          },
+          {
+            id: 3,
+            title: 'post 3',
+          },
+        ],
+      },
+    ];
+  }
+}
