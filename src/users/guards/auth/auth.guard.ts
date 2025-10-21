4a400a69531b2eaacec70b14497b24b08ce31b69
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-21 16:09:39 +0300

    Guards

--- src/users/guards/auth/auth.guard.ts
+++ src/users/guards/auth/auth.guard.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   canActivate(
-    context: ExecutionContext,
+    _context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     return true;
   }
